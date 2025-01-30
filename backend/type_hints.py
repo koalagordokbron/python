8f0e5e6d953c08d1830f282fa816b4263d9ace29
koalagordokbron <pablomorcillocuenca@gmail.com>
2025-01-30 16:38:25 +0100

    Type hints examples

---
+++ backend/type_hints.py
@@ -0,0 +1,14 @@
+### Type Hints ###
+# also called type annotanions
+# for better support from editor and tools
+
+my_str_variable = "My string variable"
+print(my_str_variable)
+print(type(my_str_variable))
+
+my_str_variable = 5
+print(my_str_variable)
+print(type(my_str_variable))
+
+typed_var: int = 5
+print(typed_var)
